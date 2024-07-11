f86e11fc94dbb5a07c36494933078d3c2602ede4
Mamun DevXhub <abdullah-mamun@devxhub.com>
2024-07-11 15:20:18 +0600

    @mdxh/vue-i18n

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>Vue i18n Wrapper Package</main>
+  <main>Vue i18n Wrapper Package {{ $t('welcome') }}</main>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
+const i18n = createI18n({
+  legacy: true,
+  locale: 'bn',
+  fallbackLocale: 'en',
+  lazy: true,
+  messages: {
+    en: {
+      welcome: 'Welcome'
+    },
+    bn: {
+      welcome: 'স্বাগতম'
+    }
+  }
+})
 const app = createApp(App)
 
+app.use(i18n)
 app.mount('#app')
